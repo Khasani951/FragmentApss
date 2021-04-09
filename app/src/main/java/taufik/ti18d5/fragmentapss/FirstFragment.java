1204bd3d25368b2727ea02923f291bc3fc09384d
Khasani951 <taufikkhasani85@gmail.com>
2021-04-09 19:49:31 +0700

    Initial commit

---
+++ app/src/main/java/taufik/ti18d5/fragmentapss/FirstFragment.java
@@ -0,0 +1,8 @@
+package taufik.ti18d5.fragmentapss;
+
+import android.app.Fragment;
+
+public class FirstFragment extends  Fragment{
+
+
+}
---
+++ app/src/main/java/taufik/ti18d5/fragmentapss/SecondActivity.java
@@ -0,0 +1,7 @@
+package taufik.ti18d5.fragmentapss;
+
+import android.app.Activity;
+
+public class SecondActivity extends Activity {
+
+}
